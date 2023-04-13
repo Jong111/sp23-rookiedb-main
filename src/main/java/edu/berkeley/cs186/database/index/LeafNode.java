08d85cd88ef180b235c6939d94087a94827370cb
HeJunting <1401607862@qq.com>
2023-04-13 21:00:31 +0800

    完成了project 2的task 1

--- src/main/java/edu/berkeley/cs186/database/index/LeafNode.java
+++ src/main/java/edu/berkeley/cs186/database/index/LeafNode.java
@@ -384,8 +384,22 @@ class LeafNode extends BPlusNode {
         // Note: LeafNode has two constructors. To implement fromBytes be sure to
         // use the constructor that reuses an existing page instead of fetching a
         // brand new one.
+        Page page = bufferManager.fetchPage(treeContext, pageNum);
+        Buffer buf = page.getBuffer();
 
-        return null;
+        byte nodeType = buf.get();
+        assert (nodeType == (byte) 1);
+
+        Optional<Long> rightSibling = Optional.of(buf.getLong());
+        List<DataBox> keys = new ArrayList<>();
+        List<RecordId> rids = new ArrayList<>();
+        int n = buf.getInt();
+        for (int i = 0; i < n; ++i) {
+            keys.add(DataBox.fromBytes(buf, metadata.getKeySchema()));
+            rids.add(RecordId.fromBytes(buf));
+        }
+        return new LeafNode(metadata, bufferManager, page, keys, rids, rightSibling, treeContext);
+        // return null;
     }
 
     // Builtins ////////////////////////////////////////////////////////////////
